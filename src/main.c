a316aec3fa18c540756b02f78663228159f92e2f
Lucas Miller <millerlucas25@gmail.com>
2026-07-19 14:42:54 -0400

    SSD1306 LCD IO Device handle allocated from i2c bus

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "freertos/FreeRTOS.h"
 #include "driver/gpio.h"
 #include "driver/i2c_master.h"
+#include "esp_lcd_io_i2c.h"
 
 static const char *TAG = "main";
 
@@ -22,6 +23,21 @@ i2c_master_bus_config_t i2c_conf = {
     .flags.enable_internal_pullup = true,
 };
 
+const int LCD_CLOCK_SPEED_HZ = 40000;
+const int LCD_COMMAND_BIT_WIDTH = 8;
+const int LCD_PARAMETER_BIT_WIDTH = 8; 
+const int LCD_CONTRL_PHASE_BYTES = 1;
+const int LCD_DC_BIT_OFFSET = 6;
+esp_lcd_panel_io_handle_t lcd_io_handle = NULL;
+esp_lcd_panel_io_i2c_config_t lcd_io_conf = {
+    .dev_addr = 0X3C,
+    .scl_speed_hz = LCD_CLOCK_SPEED_HZ,
+    .lcd_cmd_bits = LCD_COMMAND_BIT_WIDTH,
+    .lcd_param_bits = LCD_PARAMETER_BIT_WIDTH,
+    .control_phase_bytes = LCD_CONTRL_PHASE_BYTES,
+    .dc_bit_offset = LCD_DC_BIT_OFFSET,
+};
+
 void app_main()
 {
     gpio_config(&io_conf);
@@ -37,6 +53,12 @@ void app_main()
 
     ESP_ERROR_CHECK(i2c_master_probe(bus_handle, 0x3C, 1000));
     ESP_LOGI(TAG, "SSD1306 found at 0x3C!");
+
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(bus_handle, &lcd_io_conf, &lcd_io_handle));
+    ESP_LOGI(TAG, "LCD IO DEVICE ALLOCATED!");
+
+    // Wrap i2c bus in a panel-io layer
+
 
 
     vTaskDelay(pdMS_TO_TICKS(1000));
